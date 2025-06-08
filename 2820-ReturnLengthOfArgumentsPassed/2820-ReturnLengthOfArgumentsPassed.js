8ab5cee6be2fc6749775dc8a2451eda9ab3c8233
Michael Skotar <michaelskotar@gmail.com>
2025-06-07 23:33:17 -0400

    Create 2820-ReturnLengthOfArgumentsPassed

---
+++ 2820-ReturnLengthOfArgumentsPassed/2820-ReturnLengthOfArgumentsPassed.js
@@ -0,0 +1,12 @@
+// Last updated: 6/7/2025, 11:33:19 PM
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    return args.length
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
