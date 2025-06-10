a0b9213b82eab911bf37694597a4ea3d3a983d52
Michael Skotar <michaelskotar@gmail.com>
2025-06-09 22:57:21 -0400

    Create 2652-SumMultiples

---
+++ 2652-SumMultiples/version/python/2652-SumMultiples_20250609_225724.py
@@ -0,0 +1,10 @@
+# Last updated: 6/9/2025, 10:57:24 PM
+class Solution:
+    def sumOfMultiples(self, n: int) -> int:
+        sum = 0
+
+        for i in range(1, n+1):
+            if i % 3 == 0 or i % 5 == 0 or i % 7 == 0:
+                sum += i
+        
+        return sum
